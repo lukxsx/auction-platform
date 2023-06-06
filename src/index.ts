78ecd6c5fb21160f0f9ac53b7abab79169b20aff
lukxsx <33071953+lukxsx@users.noreply.github.com>
2023-06-06 19:02:53 +0300

    Install Kysely, set prettier rules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import express from "express";
 import * as dotenv from "dotenv";
+import { Pool } from "pg";
+import { Kysely, PostgresDialect, Generated } from "kysely";
 
 dotenv.config();
 const app = express();
@@ -7,11 +12,22 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 
+const db = new Kysely<Database>({
+    dialect: new PostgresDialect({
+        pool: new Pool({
+            host: "localhost",
+            database: "kysely_test",
+            user: "username",
+            password: "password",
+        }),
+    }),
+});
+
 app.get("/ping", (_req, res) => {
-  console.log("ping");
-  res.send("pong");
+    console.log("ping");
+    res.send("pong");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
