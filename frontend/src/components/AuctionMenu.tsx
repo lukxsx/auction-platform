97790c4608bf6f4433c9895d377bc880d733a621
lukxsx <33071953+lukxsx@users.noreply.github.com>
2023-08-17 17:26:13 +0300

    Added auction menu to replace sidebar

---
+++ frontend/src/components/AuctionMenu.tsx
@@ -0,0 +1,138 @@
+import { useSelector } from "react-redux";
+import { ListGroup, Badge } from "react-bootstrap";
+import { RootState, AuctionState } from "../types";
+import { formatDate } from "../utils/helpers";
+import { Link } from "react-router-dom";
+
+const AuctionMenu = () => {
+    const auctions = useSelector((state: RootState) => state.auctions);
+    const currentAuctions = auctions.filter(
+        (a) => a.state === AuctionState.Running
+    );
+    const upcomingAuctions = auctions.filter(
+        (a) => a.state === AuctionState.Pending
+    );
+    const pastAuctions = auctions.filter(
+        (a) => a.state === AuctionState.Finished
+    );
+
+    return (
+        <div>
+            {currentAuctions.length > 0 && (
+                <div>
+                    <h4>Running auctions</h4>
+                    <ListGroup className="md-3" style={{ marginBottom: "1em" }}>
+                        {currentAuctions.map((auction) => (
+                            <ListGroup.Item
+                                variant="success"
+                                key={auction.id}
+                                as={Link}
+                                to={`/auction/${auction.id}`}
+                                className="d-flex justify-content-between align-items-start"
+                            >
+                                <div className="ms-2 me-auto">
+                                    <div
+                                        className="fw-bold"
+                                        style={{
+                                            textDecorationStyle: "unset",
+                                            textDecoration: "none",
+                                        }}
+                                    >
+                                        {auction.name}
+                                    </div>
+                                    <Badge>
+                                        Start:{" "}
+                                        {formatDate(auction.start_date, false)}
+                                    </Badge>
+                                    {"  "}
+                                    <Badge>
+                                        End:{" "}
+                                        {formatDate(auction.end_date, false)}
+                                    </Badge>
+                                </div>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                </div>
+            )}
+
+            {upcomingAuctions.length > 0 && (
+                <div>
+                    <h4>Upcoming auctions</h4>
+                    <ListGroup className="md-3" style={{ marginBottom: "1em" }}>
+                        {upcomingAuctions.map((auction) => (
+                            <ListGroup.Item
+                                key={auction.id}
+                                variant="secondary"
+                                as={Link}
+                                to={`/auction/${auction.id}`}
+                                className="d-flex justify-content-between align-items-start"
+                            >
+                                <div className="ms-2 me-auto">
+                                    <div
+                                        className="fw-bold"
+                                        style={{
+                                            textDecorationStyle: "unset",
+                                            textDecoration: "none",
+                                        }}
+                                    >
+                                        {auction.name}
+                                    </div>
+                                    <Badge>
+                                        Start:{" "}
+                                        {formatDate(auction.start_date, false)}
+                                    </Badge>
+                                    {"  "}
+                                    <Badge>
+                                        End:{" "}
+                                        {formatDate(auction.end_date, false)}
+                                    </Badge>
+                                </div>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                </div>
+            )}
+
+            {pastAuctions.length > 0 && (
+                <div>
+                    <h4>Past auctions</h4>
+                    <ListGroup className="md-3" style={{ marginBottom: "1em" }}>
+                        {pastAuctions.map((auction) => (
+                            <ListGroup.Item
+                                key={auction.id}
+                                variant="dark"
+                                as={Link}
+                                to={`/auction/${auction.id}`}
+                                className="d-flex justify-content-between align-items-start"
+                            >
+                                <div className="ms-2 me-auto">
+                                    <div
+                                        className="fw-bold"
+                                        style={{
+                                            textDecorationStyle: "unset",
+                                            textDecoration: "none",
+                                        }}
+                                    >
+                                        {auction.name}
+                                    </div>
+                                    <Badge>
+                                        Start:{" "}
+                                        {formatDate(auction.start_date, false)}
+                                    </Badge>
+                                    {"  "}
+                                    <Badge>
+                                        End:{" "}
+                                        {formatDate(auction.end_date, false)}
+                                    </Badge>
+                                </div>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default AuctionMenu;
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap";
+import AuctionMenu from "./AuctionMenu";
 
 const Home = () => {
     return (
         <Container>
             <h2>Welcome to Auctions</h2>
+            <AuctionMenu />
         </Container>
     );
 };
