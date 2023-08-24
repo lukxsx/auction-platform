c7f86cf497e253205b354f7b6083a5ea0af5ce1a
lukxsx <33071953+lukxsx@users.noreply.github.com>
2023-08-24 23:45:01 +0300

    Image service for handling uploads
    Forgot to add this to image upload commit before...

---
+++ frontend/src/services/images.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+import { headers } from "../utils/helpers";
+import { BACKEND_URL } from "../utils/config";
+
+const uploadImage = async (itemId: number, formData: FormData) => {
+    const response = await axios.post(
+        `${BACKEND_URL}/images/${itemId}`,
+        formData,
+        {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                ...headers().headers,
+            },
+        }
+    );
+    return response.data;
+};
+
+export default {
+    uploadImage,
+};
